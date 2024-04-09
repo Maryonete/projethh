20eeec86c0b93686e757fb81f49286c5fe1c4389
Maryonete <mauricemarion@hotmail.com>
2024-04-09 07:06:43 +0200

    add login User

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,43 +8,67 @@ use Symfony\Component\Routing\Attribute\Route;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 use PhpOffice\PhpSpreadsheet\Reader\Exception as ReaderException;
+use PhpOffice\PhpSpreadsheet\Helper\Sample;
+use App\Entity\File;
+use App\File\FileUploader;
+use App\Form\FileType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mime\MimeTypes;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function home(EntityManagerInterface $em): Response
     {
         try {
+
+
             // Chargement du fichier
             $filePath = $this->getParameter('kernel.project_dir') . '/asso.xlsx';
+
             // Charger le fichier Excel
             $spreadsheet = IOFactory::load($filePath);
 
             // Récupérer les données de la première feuille
             $sheet = $spreadsheet->getActiveSheet();
-            $data = $sheet->toArray();
 
-            // Récupérer la lettre de la colonne la plus élevée
+            // Déterminer la dernière colonne non vide
             $highestColumn = $sheet->getHighestColumn();
 
-            // Convertir la lettre de la colonne en index de colonne
-            $highestColumnIndex = Coordinate::columnIndexFromString($highestColumn);
-
-            // Trouver la dernière colonne non vide dans la première ligne
-            for ($i = $highestColumnIndex; $i >= 1; $i--) {
-                $cellValue = $sheet->getCellByColumnAndRow($i, 1)->getValue();
-                if (!empty($cellValue)) {
-                    break;
+            // Lire les données de la feuille en excluant les colonnes vides
+            $data = array();
+            for ($row = 1; $row <= $sheet->getHighestRow(); $row++) {
+                $rowData = array();
+                for ($column = 'A'; $column <= $highestColumn; $column++) {
+                    $cell = $sheet->getCell($column . $row);
+                    $rowData[] = $cell->getValue();
                 }
+                $data[] = $rowData;
             }
+            // $form = $this->createForm(FileType::class);
+            // $form->add('save', SubmitType::class);
 
-            // Filtrer les colonnes vides
-            $data = array_map(function ($row) use ($i) {
-                return array_slice($row, 0, $i);
-            }, $data);
+            // // ajout fichier excel
+            // $form->handleRequest($request);
+            // if ($form->isSubmitted() && $form->isValid()) {
+            //     /** @var File $file */
+            //     $file = $form->getData();
+
+            //     if ($uploadedFile = $form->get('file')->getData()) {
+            //         $filename = $uploader->upload($uploadedFile);
+            //         $file->setFilename($filename);
+            //     }
+
+            //     $em->persist($file);
+            //     $em->flush();
+            // }
             return $this->render('home/index.html.twig', [
                 'controller_name' => 'HomeController',
                 'data' => $data,
+                // 'form' => $form
             ]);
         } catch (ReaderException $e) {
             // Gérer les erreurs de lecture du fichier Excel
@@ -54,4 +78,66 @@ class HomeController extends AbstractController
             echo 'Erreur : ' . $e->getMessage();
         }
     }
+
+
+
+    // #[Route('/create', name: 'create')]
+    // public function create(EntityManagerInterface $em, Request $request, FileUploader $uploader): Response
+    // {
+    //     $form = $this->createForm(FileType::class);
+    //     $form->add('save', SubmitType::class);
+
+    //     $form->handleRequest($request);
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         /** @var File $file */
+    //         $file = $form->getData();
+
+    //         if ($uploadedFile = $form->get('file')->getData()) {
+    //             $filename = $uploader->upload($uploadedFile);
+    //             $file->setFilename($filename);
+    //         }
+
+    //         $em->persist($file);
+    //         $em->flush();
+    //     }
+
+    //     return $this->render('pages/file/edit.html.twig', [
+    //         'form' => $form
+    //     ]);
+    // }
+
+    // #[Route('/display-image/{filename}', name: 'display_image')]
+    // public function displayImage(string $filename = null)
+    // {
+    //     //        if (!$this->getUser()->getRoles()) {
+    //     //            return new Response();
+    //     //        }
+
+    //     if ($filename === null) {
+    //         return new Response();
+    //     }
+
+    //     $image = $this->getParameter('upload_directory') . '/' . $filename;
+
+    //     if (!file_exists($image)) {
+    //         return new Response();
+    //     }
+    //     //        test later is it's not an image do not display
+    //     //        if (!in_array($this->getMimeType($image), ['jpeg', 'png', 'svg'])) {
+    //     //            return new Response();
+    //     //        }
+
+    //     header("Content-type: " . $this->getMimeType($image));
+    //     header("Content-Disposition: inline; filename=" . basename($image));
+
+    //     readfile($image);
+
+    //     return new Response();
+    // }
+
+    // private function getMimeType($file): string
+    // {
+    //     $guesser = new MimeTypes();
+    //     return $guesser->guessMimeType($file);
+    // }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,15 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
+    .copyFiles({
+
+        from: './assets/images',
+        
+        to: 'images/[path][name].[ext]',
+        
+        })
+        
 ;
 
 module.exports = Encore.getWebpackConfig();
