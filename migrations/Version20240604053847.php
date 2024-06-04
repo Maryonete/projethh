215d9c480d6a366632b1cf3c9b8edc9d95eeda42
Maryonete <mauricemarion@hotmail.com>
2024-06-04 07:39:32 +0200

    add migration admin

--- migrations/Version20240604053847.php
+++ migrations/Version20240604053847.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240604051956 extends AbstractMigration
+final class Version20240604053847 extends AbstractMigration
 {
     public function getDescription(): string
     {
